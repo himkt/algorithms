84fd1d7dc775e4968b3c87ae16f7dd6ba956d0c0
himkt <himkt@cookpad.com>
2019-06-08 23:11:56 +0900

    Create algorithms

---
+++ setup.py
@@ -0,0 +1,16 @@
+"""setup.py."""
+
+
+from setuptools import find_packages
+from setuptools import setup
+
+
+setup(
+    name='algorithms',
+    version='0.0.1',
+    description='Algorithm implementations for learning',
+    author='himkt',
+    author_email='himkt@klis.tsukuba.ac.jp',
+    url='https://github.com/himkt/algorithms',
+    packages=find_packages()
+)
